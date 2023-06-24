d83762e99dfbb7094c34facbaa33ba6fa404dc17
qzhong0605 <qzhong2013@gmail.com>
2023-06-24 12:11:58 +0800

    refactor code

--- src/osdep/path.h
+++ src/osdep/path.h
@@ -1,96 +1,32 @@
-/*
- * Get path to config dir/file.
- *
- * This file is part of mpv.
- *
- * mpv is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * mpv is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with mpv.  If not, see <http://www.gnu.org/licenses/>.
- */
+#ifndef OSDEP_PATH_H
+#define OSDEP_PATH_H
 
-#ifndef MPLAYER_PATH_H
-#define MPLAYER_PATH_H
+// Return a platform-specific path, identified by the type parameter. If the
+// return value is allocated, talloc_ctx is used as talloc parent context.
+//
+// The following type values are defined:
+//  "home"          the native mpv-specific user config dir
+//  "old_home"      same as "home", but lesser priority (compatibility)
+//  "osxbundle"     OSX bundle resource path
+//  "global"        the least priority, global config file location
+//  "desktop"       path to desktop contents
+//
+//  These additional types are also defined. However, they are not necessarily
+//  implemented on every platform. Unlike some other type values that are
+//  platform specific (like "osxbundle"), the value of "home" is returned
+//  instead if these types are not explicitly defined.
+//  "cache"         the native mpv-specific user cache dir
+//  "state"         the native mpv-specific user state dir
+//
+// It is allowed to return a static string, so the caller must set talloc_ctx
+// to something other than NULL to avoid memory leaks.
+typedef const char *(*mp_get_platform_path_cb)(void *talloc_ctx, const char *type);
 
-#include <stdbool.h>
-#include "misc/bstr.h"
+// Conforming to mp_get_platform_path_cb.
+const char *mp_get_platform_path_darwin(void *talloc_ctx, const char *type);
+const char *mp_get_platform_path_uwp(void *talloc_ctx, const char *type);
+const char *mp_get_platform_path_win(void *talloc_ctx, const char *type);
+const char *mp_get_platform_path_osx(void *talloc_ctx, const char *type);
+const char *mp_get_platform_path_unix(void *talloc_ctx, const char *type);
 
-struct mpv_global;
-struct MPOpts;
-
-void mp_init_paths(struct mpv_global *global, struct MPOpts *opts);
-
-// Search for the input filename in several paths. These include user and global
-// config locations by default. Some platforms may implement additional platform
-// related lookups (i.e.: OSX inside an application bundle).
-char *mp_find_config_file(void *talloc_ctx, struct mpv_global *global,
-                          const char *filename);
-
-// Search for local writable user files within a specific kind of user dir
-// as documented in osdep/path.h. This returns a result even if the file does
-// not exist. Calling it with filename="" is equivalent to retrieving the path
-// to the dir.
-char *mp_find_user_file(void *talloc_ctx, struct mpv_global *global,
-                        const char *type, const char *filename);
-
-// Find all instances of the given config file. Paths are returned in order
-// from lowest to highest priority. filename can contain multiple names
-// separated with '|', with the first having highest priority.
-char **mp_find_all_config_files(void *talloc_ctx, struct mpv_global *global,
-                                const char *filename);
-
-// Normally returns a talloc_strdup'ed copy of the path, except for special
-// paths starting with '~'. Used to allow the user explicitly reference a
-// file from the user's home or mpv config directory.
-char *mp_get_user_path(void *talloc_ctx, struct mpv_global *global,
-                       const char *path);
-
-// Return pointer to filename part of path
-
-char *mp_basename(const char *path);
-
-/* Return file extension, excluding the '.'. If root is not NULL, set it to the
- * part of the path without extension. So: path == root + "." + extension
- * Don't consider it a file extension if the only '.' is the first character.
- * Return NULL if no extension and don't set *root in this case.
- */
-char *mp_splitext(const char *path, bstr *root);
-
-/* Return struct bstr referencing directory part of path, or if that
- * would be empty, ".".
- */
-struct bstr mp_dirname(const char *path);
-
-void mp_path_strip_trailing_separator(char *path);
-
-/* Join two path components and return a newly allocated string
- * for the result. '/' is inserted between the components if needed.
- * If p2 is an absolute path then the value of p1 is ignored.
- */
-char *mp_path_join(void *talloc_ctx, const char *p1, const char *p2);
-char *mp_path_join_bstr(void *talloc_ctx, struct bstr p1, struct bstr p2);
-
-// Return whether the path is absolute.
-bool mp_path_is_absolute(struct bstr path);
-
-char *mp_getcwd(void *talloc_ctx);
-
-bool mp_path_exists(const char *path);
-bool mp_path_isdir(const char *path);
-
-bool mp_is_url(bstr path);
-
-bstr mp_split_proto(bstr path, bstr *out_url);
-
-void mp_mkdirp(const char *dir);
-void mp_mk_user_dir(struct mpv_global *global, const char *type, char *subdir);
-
-#endif /* MPLAYER_PATH_H */
+#endif
